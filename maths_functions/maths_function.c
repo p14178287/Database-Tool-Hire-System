e3058914294d3b0636804233830686e4dce2b5bf
Shingirai Marikasi <P14178287@GWHXX582LIN016W.lec-admin.dmu.ac.uk>
2016-01-19 15:13:03 +0000

    maths_function.c

---
+++ maths_functions/maths_function.c
@@ -0,0 +1,22 @@
+#include <stdio>
+int main ()
+{
+char o;
+int a, b;
+
+printf("enter choice\n");
+scanf("%c", &a);
+
+/*int add (a, b){
+return a + b;
+}
+
+int subtract(a, b){
+return a - b;
+}
+
+int multiply(a, b){
+return a * b;
+}
+*/
+
